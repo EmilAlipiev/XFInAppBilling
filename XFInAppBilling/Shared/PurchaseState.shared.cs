86f88569591cca948d5357ce921acec978d02122
Emil Alipiev <emil_alipiev@hotmail.com>
2020-02-09 16:06:56 +0100

    purchase states extended

--- XFInAppBilling/Shared/PurchaseState.shared.cs
+++ XFInAppBilling/Shared/PurchaseState.shared.cs
@@ -4,20 +4,63 @@ using System.Text;
 
 namespace Plugin.XFInAppBilling
 {
+    /// <summary>
+    /// Status used for purchase and consume
+    /// </summary>
     [Preserve(AllMembers = true)]
     public enum PurchaseState
     {
+        /// <summary>
+        /// unknown
+        /// </summary>
         Unspecified = 0,
+        /// <summary>
+        /// successful purchase or consume
+        /// </summary>
         Purchased = 1,
+        /// <summary>
+        /// incomplete
+        /// </summary>
         Pending = 2,
+        /// <summary>
+        /// not successfull
+        /// </summary>
         Failed = 3,
+        /// <summary>
+        /// cancelled
+        /// </summary>
         Cancelled = 4,
+        /// <summary>
+        /// pending
+        /// </summary>
         NotAknowledged = 5,
+        /// <summary>
+        /// restored
+        /// </summary>
         Restored = 6,
+        /// <summary>
+        /// refunded
+        /// </summary>
         Refunded = 7,
+        /// <summary>
+        /// in purchase progress
+        /// </summary>
         Purchasing = 8,
+        /// <summary>
+        /// trial purchase
+        /// </summary>
         FreeTrial = 9,
+        /// <summary>
+        /// pament not confirmed
+        /// </summary>
         PaymentPending = 10,
-        AlreadyOwned = 11
+        /// <summary>
+        /// purchase already owned by user
+        /// </summary>
+        AlreadyOwned = 11,
+        /// <summary>
+        /// used for consume only
+        /// </summary>
+        InsufficentQuantity = 12
     }
 }
